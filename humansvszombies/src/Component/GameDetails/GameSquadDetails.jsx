ac1a6b02715ee0a89315c666fcb910b3d679733c
Negin-Bakhtiarirad <109150935+Negin-Bakhtiarirad@users.noreply.github.com>
2022-09-15 16:18:27 +0200

    Feat: adding squadmember list in squad details

--- humansvszombies/src/Component/GameDetails/GameSquadDetails.jsx
+++ humansvszombies/src/Component/GameDetails/GameSquadDetails.jsx
@@ -1,17 +1,46 @@
-const GameSquadDetails = () => {
+import { useEffect, useState } from "react"
+import { getPlayersInGame } from "../../api/game";
+import { getSquadsInGame } from "../../api/squad";
+
+const GameSquadDetails = ({game}) => {
+//Hooks
+const [squads, setSquads] = useState([]);
+const [ apiError, setApiError] = useState(null);
+const gameId = game.gameId;
+
+
+//Get the players for the specific game
+useEffect(() => {
+    const getSquads = async () => {
+        const [ error, userResponse ] = await getSquadsInGame(gameId);
+        if (error !== null){
+            setApiError(error)
+        }
+        if(userResponse !== null){
+            setSquads(userResponse)
+        }
+    }
+    getSquads();
+    },[gameId]);
+
     return(
         <>
-            <div id="gameSquadDetails" className="text-center">
+        <div id="SquadList" className="text-center">
                 <header>
-                    <h2>Squad Details</h2>
+                    <h2>Squad details</h2>
                 </header>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Andreas Persson - rank- state</li>
-                    <li class="list-group-item">Ebba Eliasson - rank- state</li>
-                    <li class="list-group-item">Adrian Ericsson - rank- state</li>
+                <ul>
+                {squads.map(s =>
+                        <li key={s.squadMembers}>
+                        <div class="card w-50" id="card">
+                            <div class="card-body">
+                                <h5 class="card-title">{s.squadMembers}</h5>
+                            </div>
+                        </div>
+                    </li>    
+                    )}
                 </ul>
-            </div>
-        </>
+            </div></>
     )
 }
 export default GameSquadDetails
